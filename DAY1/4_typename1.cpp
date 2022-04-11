3f7b0aca6d11a8e873e61cbe047acf931c7a4f84
ecourse <smkang@codenuri.co.kr>
2022-04-11 12:00:44 +0900

    Add files via upload

--- DAY1/4_typename1.cpp
+++ DAY1/4_typename1.cpp
@@ -18,7 +18,12 @@ template<typename T> void foo(T a)
 	// 아래 2줄을 생각해 보세요
 	// "T::이름" 처럼 표기하는 컴파일러는 "이름"을 무조건 "값"으로 해석합니다.
 	T::value * p;  // ok
-	T::DWORD * p;  // error
+//	T::DWORD * p;  // error 
+
+	typename T::DWORD* p; // ok
+
+	// 결론 : 템플릿 인자에 의존해서 이름을 꺼낼때.. 이름이 값이 아닌 타입이라면
+	//		 반드시 앞에 "typename"을 적어야 합니다.
 }
 
 
