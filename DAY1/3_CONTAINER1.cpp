cc53604e673e85fae7d0ea3e1894ed875632196b
ecourse <smkang@codenuri.co.kr>
2022-04-11 10:42:36 +0900

    Add files via upload

---
+++ DAY1/3_CONTAINER1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// 싱글 링크드 리스트 입니다
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& a, Node* n) : data(a), next(n) {}
+};
+
+template<typename T> class slist
+{
+	Node<T>* head = 0;
+public:
+	// 싱글리스트는 아래 처럼 생성자만 잘 활용하면
+	// 간단하게 만들수 있습니다.
+	void push_front(const T& a)
+	{
+		head = new Node<T>(a, head);
+	}
+};
+
+int main()
+{
+	// 31 page 그림 참고 하세요
+	slist<int> s;
+	
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+	s.push_front(50);
+}
