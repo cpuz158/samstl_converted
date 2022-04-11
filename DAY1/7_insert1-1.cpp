f0ac391bdc0f19adce94f46d328982e798c66abe
ecourse <smkang@codenuri.co.kr>
2022-04-11 16:44:18 +0900

    Add files via upload

--- DAY1/7_insert1-1.cpp
+++ DAY1/7_insert1-1.cpp
@@ -13,6 +13,7 @@ T square(T a)
 // 
 // 이 경우는 클래스 템플릿 의 객체를 만드는  함수 템플릿을 제공하면 편리합니다
 
+// Object Generator 라는 이름을 가진 기술 입니다.
 template<typename T, typename U>
 std::pair<T, U> make_pair(const T& a, const U& b)
 {
@@ -27,7 +28,14 @@ int main()
 
 	std::pair<int, double> p1(3, 3.4); // int, double을 생략할수 있을까 ?
 
-	auto p2 = make_pair(3, 3.4);
+//	auto p2 = make_pair<int, double>(3, 3.4); // 이 표현이 정확한 표현이지만
+//	auto p2 = make_pair(3, 3.4); // 이렇게 해도 됩니다.
+
+	auto p2 = std::make_pair(3, 3.4);
+
+
+	std::pair p3(3, 3.4); // C++17 부터는 클래스(구조체)템플릿도
+							// 타입 인자 생략 가능합니다
 }
 
 
