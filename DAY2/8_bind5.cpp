d666563a662c67a05a5c11d7bb457a3a396a18a6
ecourse <smkang@codenuri.co.kr>
2022-04-12 17:12:33 +0900

    Add files via upload

--- DAY2/8_bind5.cpp
+++ DAY2/8_bind5.cpp
@@ -3,17 +3,43 @@
 #include <functional>
 using namespace std::placeholders;
 
-class Button
+class Machine
 {
 public:
+	void mf1(int a) { std::cout << "mf1" << std::endl; }
+	void mf2(int a, int b) {  std::cout << "mf2" << std::endl; } 
+};
+void f0() { printf("f0\n"); }
+void f1(int a) { printf("f1 : %d\n", a); }
+
+class Button
+{
+	using HANDLER = std::function<void()>; 
+
+	std::vector< HANDLER > v;
+public:
+	Button(HANDLER h = 0 )
+	{
+		if (h != 0)
+			v.push_back(h);
+	}
+	void addHandler(HANDLER h) { v.push_back(h); }
+
 	void click()
 	{
-		// 버튼이 눌린 사실을 위부에 알려야 합니다.
+		for (auto f : v) 
+			f();  // f 는 결국 function 이므로 ()로 호출하면 됩니다.
 	}
 };
 
 int main()
 {
-	Button b1, b2; // 2개의 버튼 만들기
+	Button b1(&f0);
+	Button b2(std::bind(&f1, 2));
+	Button b3(std::bind(&f1, 3));
+
+	b1.addHandler([]() { std::cout << "lambda" << std::endl; });
+	b1.addHandler(std::bind(&Machine::mf1, Machine(), 10));
+
 	b1.click();
 }
