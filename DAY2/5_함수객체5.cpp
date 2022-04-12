3bd036740300d241c9f225d3054d4741a15d1803
ecourse <smkang@codenuri.co.kr>
2022-04-12 12:16:11 +0900

    Add files via upload

--- DAY2/5_함수객체5.cpp
+++ DAY2/5_함수객체5.cpp
@@ -4,6 +4,10 @@
 
 // 137page 아래부분
 
+// 아래 Sort()는 비교 정책을 교체 할수 있지만,
+// 비교 정책의 인라인 치환이 되지 않습니다. - 느립니다.
+
+// "STL 에서는 비교 정책을 교체하는데 속도를 빠르게 하고 싶었습니다."
 
 void Sort(int* x, int sz , bool(*cmp)(int, int) )
 {
@@ -12,6 +16,7 @@ void Sort(int* x, int sz , bool(*cmp)(int, int) )
 		for (int j = i + 1; j < sz; j++)
 		{
 		//	if (x[i] > x[j])  
+
 			if ( cmp(x[i], x[j]) == false )
 				std::swap(x[i], x[j]);
 		}
@@ -19,15 +24,14 @@ void Sort(int* x, int sz , bool(*cmp)(int, int) )
 }
 //----------------
 // Sort 에 전달할 비교 정책 함수
-bool cmp1(int a, int b) { return a < b; }
-bool cmp2(int a, int b) { return a > b; }
+inline bool cmp1(int a, int b) { return a < b; }
+inline bool cmp2(int a, int b) { return a > b; }
 
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
 //	Sort(x, 10, cmp1);
 	Sort(x, 10, cmp2);
-
 	show(x);
 }
 
