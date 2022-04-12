021d991281c6bf3911d5a6007f6dd72d6e73e3c7
ecourse <smkang@codenuri.co.kr>
2022-04-12 11:06:24 +0900

    Add files via upload

--- DAY2/4_알고리즘과함수3.cpp
+++ DAY2/4_알고리즘과함수3.cpp
@@ -21,10 +21,15 @@ int main()
 //        foo);
 
     // 2. 요즘은 함수 보다는 "람다표현식"을 사용하는 것이 좋습니다.
+    // 람다표현식 : C++11부터 지원되는 문법
+    //            "익명의 함수"를 만드는 문법
     std::transform(s1.begin(), s1.end(),
         s2.begin(),
         std::back_inserter(s3),
         [](int a, int b) { return a + b; } );
+
+    // 권장 : 진짜 함수로 전달하는것 보다, 람다 표현식이 빠릅니다.
+    //       람다 표현식 사용하세요..
 
     show(s3);
 
