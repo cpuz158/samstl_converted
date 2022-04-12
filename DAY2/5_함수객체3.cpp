2ce098e054b27be5829f422a21a858fdb9021b35
ecourse <smkang@codenuri.co.kr>
2022-04-12 11:41:56 +0900

    Add files via upload

--- DAY2/5_함수객체3.cpp
+++ DAY2/5_함수객체3.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <bitset>  // STL 이 제공하는 "비트"를 관리하는 클래스
 
+// 일반 함수는 "상태를 가질수 없지만"
+// 함수 객체는 "상태를 가질수 있습니다." => 멤버 데이타를 만들수 있다는 의미.
 
 class URandom
 {
+	std::bitset<10> bs;
+	bool recycle;
 public:
+	URandom(bool b = false) : recycle(b)
+	{
+//		bs.set(5); // 5번째 비트만 1로
+		bs.set();  // 모든 비트를 1로
+	}
+
 	int operator()()
 	{
 		return rand() % 10;
 	}
 };
+
 
 URandom urand; // 이제, urand 는 함수가 아닌 객체입니다.
 			   // 하지만, 함수처럼 사용가능합니다.
