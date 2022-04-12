0ac794c0a9370db2159c0c36505691d0f8f1d547
ecourse <smkang@codenuri.co.kr>
2022-04-12 15:07:19 +0900

    Add files via upload

---
+++ DAY2/6_조건자.cpp
@@ -0,0 +1,11 @@
+// 6_Á¶°ÇÀÚ
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,6,4,5,3,7,8,9,10 };
+
+	auto ret1 = std::find(v.begin(), v.end(), 3);
+}
