c9571ca53beb137b4fef1fd502c1941fcedc0875
ecourse <smkang@codenuri.co.kr>
2022-04-12 15:55:04 +0900

    Add files via upload

--- DAY2/7_알고리즘의정책변경2.cpp
+++ DAY2/7_알고리즘의정책변경2.cpp
@@ -10,7 +10,14 @@ int main()
 	std::vector<int> v1 = { 1,2,3,4,5,6,7,8,9,10 };
 	std::vector<int> v2;
 
-	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2));
+//	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2) );
+
+//	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2), 
+//													std::multiplies<int>());
+
+	std::adjacent_difference(v1.begin(), v1.end(),
+							std::back_inserter(v2) ); // 기본 연산 - 
+
 
 	show(v2);
 }
