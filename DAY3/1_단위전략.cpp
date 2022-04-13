a6ea8cd5a12b84a21bb1f2fad03e227170a05f01
ecourse <smkang@codenuri.co.kr>
2022-04-13 09:08:34 +0900

    Add files via upload

--- DAY3/1_단위전략.cpp
+++ DAY3/1_단위전략.cpp
@@ -1,7 +1,21 @@
+// Policy Base Design ( 단위 전략 디자인 )
+
+// 클래스가 사용하는 "어떤 정책" 을 템플릿 인자를 통해서 전달하는 것.
+
+// 템플릿 인자로 전달되는 정책 클래스는 지켜야 하는 규칙을 가지게 됩니다.
+// => 아래 코드에서는 "lock()/unlock()" 함수가 있어야 합니다.
+// => 이런 규칙을 모아 놓은 것을 "named requirement" 라고 합니다.
+
+// STL 은 "단위전략 디자인"을 아주 널리 사용합니다.
+
+// 단, STL 컨테이너는 멀티스레드 동기화는 되지 않습니다.
+// => 아래 처럼 되어 있지는 않습니다.
+
+
 
 // 아래 클래스 에서 동기화 여부는 결국
 // list 사용자가 템플릿 2번째 인자로 전달하는 타입에 따라 달라집니다.
-template<typename T, typename ThreadModel > class List
+template<typename T, typename ThreadModel = noLock> class List
 {	
 	ThreadModel tm;
 public:
@@ -29,7 +43,9 @@ public:
 };
 //---------------------------------------
 // 이제 List 사용자는 template  인자로 "동기화 정책"을 전달 합니다
-List<int, noLock> st;	// 전역변수는 멀티스레드 환경에서 안전하지 않습니다.
+//List<int, noLock> st;	// 전역변수는 멀티스레드 환경에서 안전하지 않습니다.
+//List<int, mutexLock> st;
+List<int> st;
 
 int main()
 {
