5a82e2088adddb3f4edf53542b7a68157f9359a9
ecourse <smkang@codenuri.co.kr>
2022-04-13 11:41:56 +0900

    Add files via upload

--- DAY3/3_선형컨테이너3-1.cpp
+++ DAY3/3_선형컨테이너3-1.cpp
@@ -6,7 +6,12 @@
 int main()
 {
 	// 사용자가 -1을 입력할때 까지 계속 입력 받아서 보관하고 싶다.
+//	std::vector<int> v(100); // 이렇게 해 놓면 
+							 // 항상 size = 100 이므로 
+							 // 몇개 입력 되었는지 파악이 안됩니다.
+
 	std::vector<int> v;
+	v.reserve(100); // 100 개를 미리 확보..
 
 	int num = 0;
 
@@ -18,6 +23,7 @@ int main()
 
 		v.push_back(num);
 	}
+	std::cout << v.size() << std::endl;
 
 }
 
