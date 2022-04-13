2a11dea3fef6bdd74939f3a90d41feb1725227bd
ecourse <smkang@codenuri.co.kr>
2022-04-13 11:47:51 +0900

    Add files via upload

--- DAY3/3_선형컨테이너3-2.cpp
+++ DAY3/3_선형컨테이너3-2.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include "show.h"
 
+// 아래 코드를 보고
+// std::remove()   vs   v.erase()   vs   v.resize()
 int main()
 {
 	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
@@ -12,10 +14,15 @@ int main()
 
 	std::cout << v.size() << ", " << v.capacity() << " : "; show(v);
 
+	// 아래 코드는 p~ v.end() 를 제거하는 코드 입니다.
 	v.erase(p, v.end());
 
 	std::cout << v.size() << ", " << v.capacity() << " : "; show(v);
 
+	// 실제 메모리를 줄이려면, resize() 또는 shrink_to_fit
+	v.shrink_to_fit(); // v.resize( v.size() ) 의 의미
+
+	std::cout << v.size() << ", " << v.capacity() << " : "; show(v);
 }
 
 
