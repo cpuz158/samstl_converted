fb9405ee72e892d30dcf7831bdf576f90e0b6129
ecourse <smkang@codenuri.co.kr>
2022-04-13 11:08:29 +0900

    Add files via upload

--- DAY3/3_선형컨테이너3.cpp
+++ DAY3/3_선형컨테이너3.cpp
@@ -10,10 +10,27 @@ int main()
 
 	v.resize(7); // 어떻게 구현했을까요 ?
 
-	std::cout << v.size() << std::endl;
-	std::cout << v.capacity() << std::endl;
+	std::cout << v.size()     << std::endl;	// 7
+	std::cout << v.capacity() << std::endl;	// 10
 
-	
+	v.push_back(20); // 끝에 한개 추가.
+					 // 현재 상태는 메모리의 여유 공간이 있으므로
+					 // 메모리 재할당 필요 없습니다.
+					 // not expensive operation
+
+	std::cout << v.size()     << std::endl; // 8
+	std::cout << v.capacity() << std::endl; // 10
+
+	v.shrink_to_fit(); // size == capacity 가 되도록 해달라.
+
+	std::cout << v.size() << std::endl;     // 8
+	std::cout << v.capacity() << std::endl; // 8
+
+	v.push_back(3);  // 끝에 한개(3) 추가
+
+	std::cout << v.size() << std::endl;     // 9
+	std::cout << v.capacity() << std::endl; // ???
+
 }
 
 
