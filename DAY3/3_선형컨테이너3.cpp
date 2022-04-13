41d1943397fdabd8341d306acf33f595d8bc2b97
ecourse <smkang@codenuri.co.kr>
2022-04-13 11:03:44 +0900

    Add files via upload

---
+++ DAY3/3_선형컨테이너3.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "show.h"
+
+// capacity 개념
+int main()
+{
+	std::vector<int> v(10, 0);
+
+	v.resize(7); // 어떻게 구현했을까요 ?
+
+	std::cout << v.size() << std::endl;
+	std::cout << v.capacity() << std::endl;
+
+	
+}
+
+
+
+
