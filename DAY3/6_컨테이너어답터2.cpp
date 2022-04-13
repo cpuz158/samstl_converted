d5a39e1511993b01923bb775c3a0cdb505dbd4e1
ecourse <smkang@codenuri.co.kr>
2022-04-13 15:46:44 +0900

    Add files via upload

--- DAY3/6_컨테이너어답터2.cpp
+++ DAY3/6_컨테이너어답터2.cpp
@@ -3,6 +3,28 @@
 #include <queue> // queue, priority_queue
 #include "show.h"
 
+template<typename T> class debug_alloc
+{
+public:
+	T* allocate(std::size_t sz)
+	{
+		T* ptr = static_cast<T*>(malloc(sizeof(T) * sz));
+
+		printf("%d allocated at %p\n", sz, ptr);
+		return ptr;
+	}
+	void deallocate(T* ptr, std::size_t sz)
+	{
+		free(ptr);
+		printf("%d deallocated at %p\n", sz, ptr);
+	}
+
+	// 위 2개위에도 아래 3개가 있어야 합니다.
+	using value_type = T;
+	debug_alloc() {}
+	template<typename U> debug_alloc(const U&) {} // 템플릿 생성자라는 함수
+};
+
 int main()
 {
 	std::stack<int> s1;
@@ -13,7 +35,7 @@ int main()
 
 	// 질문 : stack s3를 만드세요
 	// 1. 내부 컨테이너 vector로 하세요
-	// 2. 단위전략3-1에 있는 "debug_alloc" 을 사용해 보세요.
+	// 2. 단위전략2-1에 있는 "debug_alloc" 을 사용해 보세요.
 	
 	std::stack< ? > s3;
 
