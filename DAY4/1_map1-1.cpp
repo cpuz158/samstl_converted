c0e73f081000dfd7393fefd5988aeae47527df8b
ecourse <smkang@codenuri.co.kr>
2022-04-14 10:16:37 +0900

    Add files via upload

--- DAY4/1_map1-1.cpp
+++ DAY4/1_map1-1.cpp
@@ -10,8 +10,21 @@ int main()
 	m.emplace("mon", "월요일");
 	m.emplace("sun", "일요일");
 
-	for (auto e : m)
+	// map 과 range-for
+	for (const auto& e : m)
 	{
-		
+		std::cout << e.first << std::endl;
+	}
+
+	// C++17 부터 가능한 문법
+	std::pair<int, int> p(1, 2);
+
+	auto [a, b] = p; // auto a = p.first
+					 // auto b = p.second;
+
+	for (auto [key, value] : m)
+	{
+		std::cout << key   << std::endl;
+		std::cout << value << std::endl;
 	}
 }
