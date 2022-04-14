118d18565f2e888aa2e5bc2bc6637b6d3cd45a98
ecourse <smkang@codenuri.co.kr>
2022-04-14 14:49:13 +0900

    Add files via upload

--- DAY4/4_스마트포인터7.cpp
+++ DAY4/4_스마트포인터7.cpp
@@ -17,6 +17,9 @@ int main()
 	std::shared_ptr<People> sp1(new People("kim"));
 	std::shared_ptr<People> sp2(new People("lee"));
 
+	// shared_ptr 사용시
+	// 상호 참조, 순환 참조(cycle reference) 발생시
+	// 자원은 파괴되지 않습니다.
 	sp1->bf = sp2;
 	sp2->bf = sp1;
 }
