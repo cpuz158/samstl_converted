43763825b4ea503a72690df30cd06bb127e3bfab
ecourse <smkang@codenuri.co.kr>
2022-04-14 17:17:19 +0900

    Add files via upload

--- DAY4/nullptr1.cpp
+++ DAY4/nullptr1.cpp
@@ -21,4 +21,21 @@ int main()
 
 	goo(NULL);	// void* => char* 로의 변환이 필요 합니다.
 				// C언어는 되지만 C++ 언어는 안됩니다.
+				// #define NULL (void*)0  이라면 error
+				// #define NULL 0		  이라면 ok
 }
+
+/*
+void hoo(char* p)
+{
+}
+int main()
+{
+	hoo(0); // ok   리터럴 0은 포인터로 변환가능
+
+	int n = 0;
+	hoo(n);  // error. 0으로 초기화된 int 변수는 error
+
+	hoo((int)0);
+}
+*/
