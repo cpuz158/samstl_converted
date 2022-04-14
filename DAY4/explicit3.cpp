b48e6b8250f6ccc9c699f52b30d15f5a074c4242
ecourse <smkang@codenuri.co.kr>
2022-04-14 16:15:57 +0900

    Add files via upload

--- DAY4/explicit3.cpp
+++ DAY4/explicit3.cpp
@@ -6,6 +6,16 @@ void push_back( std::shared_ptr<int> sp )
 
 int main()
 {
-	std::shared_ptr<int> sp1 = new int;
-	std::shared_ptr<int> sp2(new int);
+	// 1. shared_ptr 생성자는 explicit 입니다.
+	std::shared_ptr<int> sp1 = new int; // error
+	std::shared_ptr<int> sp2(new int);  // ok
+
+	// 2. 
+	push_back(new int); // std::shared_ptr<int> sp = new int; 이므로 error
+	push_back(std::shared_ptr<int>(new int));
+			// std::shared_ptr<int> sp = std::shared_ptr<int>(new int);
+			//	ok.. 위 코드는 복사 생성자 사용. explicit 아님.
+
+	push_back( std::make_shared<int>(0) );
+		// std::shared_ptr<int> sp = std::make_shared<int>(0)
 }
