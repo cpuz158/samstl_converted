7360e65e37c611c54c16e9afd320a2bd38bd4473
ecourse <smkang@codenuri.co.kr>
2022-04-14 11:22:24 +0900

    Add files via upload

--- DAY4/컨테이너정리.cpp
+++ DAY4/컨테이너정리.cpp
@@ -19,5 +19,18 @@
 // boost 라이브러리에서 안전성과 필요성이 검증되면 
 // 추후 C++ 표준에 추가되게 됩니다.
 
+// set, map : 정렬이 필요 한데, 검색도 빨라야 한다. => IndexMap 예제
+// unordered : 정렬 필요 없고, 검색이 제일 중요 한다.
+/*
+unordered_map<std::string, int> macro;
 
+#define MAX 10  // #define 을 만나면 macro["MAX"] = 10
+
+int main()
+{
+	int n = MAX;
+}
+*/
+// 전처리기 만들어 보세요. => 정렬 필요 없고 검색이 제일 중요 하므로
+//							hash 로 하면 됩니다.
 
