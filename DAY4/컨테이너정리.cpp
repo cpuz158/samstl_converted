3579c9f936592a418205499141d87364b9da2f15
ecourse <smkang@codenuri.co.kr>
2022-04-14 11:18:05 +0900

    Add files via upload

---
+++ DAY4/컨테이너정리.cpp
@@ -0,0 +1,23 @@
+// 컨테이너정리
+
+// sequence container 
+// => vector, list, deque,     C++98
+// => array, forward_list      C++11
+// => string 을 이 분류에 넣기도 합니다.
+
+// container adapter 3개 ( sequence 의 함수이름을 변경하는 도구)
+// => stack, queue, priority_queue
+
+// associative container
+// => set, map					   : tree 기반
+// => unordered_set, unordered_map : hash 기반
+
+//-------------------------------------------
+// graph 등이 아직 없습니다.
+// 
+// boost.org 에서 boost 라이브러리를 설치하면 더 많은 컨테이너를 사용가능
+// boost 라이브러리에서 안전성과 필요성이 검증되면 
+// 추후 C++ 표준에 추가되게 됩니다.
+
+
+
