16fa3a2884ec3af829b91a79253aca4ed059b3a1
ecourse <smkang@codenuri.co.kr>
2022-04-15 14:50:30 +0900

    Add files via upload

--- DAY5/6_optional.cpp
+++ DAY5/6_optional.cpp
@@ -8,7 +8,10 @@ int main()
 	int n1 = 0; // n1 은 정수값을 가집니다.
 
 //	std::optional<int> n2 = 10; // n2 는 정수값 + 값없음(null) 상태를 가질수 있습니다.
-	std::optional<int> n2 = std::nullopt; 
+//	std::optional<int> n2 = std::nullopt; 
+	std::optional<int> n2 = NULL; // ok.. 그런데, 0으로 취급. 즉, 값 있음.
+//	std::optional<int> n2 = nullptr;
+
 	
 	if (n2)
 	{
