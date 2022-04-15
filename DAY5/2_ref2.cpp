8eb495d0930fc55790e247fe5f7aacf9c2ef269f
ecourse <smkang@codenuri.co.kr>
2022-04-15 09:13:25 +0900

    Add files via upload

--- DAY5/2_ref2.cpp
+++ DAY5/2_ref2.cpp
@@ -18,8 +18,12 @@ int main()
 	reference_wrapper<int> r1(n1);
 	reference_wrapper<int> r2(n2);
 
-	r1 = r2;	
+	r1 = r2; // r1, r2 모두 reference_wrapper 타입이므로
+			 // reference_wrapper의 대입 연산자 호출
+			 // 컴파일러가 생성하는 대입연산자 사용
+
 	int& r3 = r1; // r1.operator int&()
+				  
 
 	std::cout << n1 << std::endl; // 20		10
 	std::cout << n2 << std::endl; // 20		20
