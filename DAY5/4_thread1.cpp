16cfd045c4d362117c525cfef00dd2b4328e0d6e
ecourse <smkang@codenuri.co.kr>
2022-04-15 10:39:35 +0900

    Add files via upload

--- DAY5/4_thread1.cpp
+++ DAY5/4_thread1.cpp
@@ -3,7 +3,7 @@
 // C++11 부터 스레드 관련 클래스 지원.
 #include <thread> 
 #include <chrono>
-using namespace std::literals;
+//using namespace std::literals;
 
 // this_thread namespace 
 // => 4개의 간단한 함수 제공
@@ -12,8 +12,10 @@ int main()
 	std::cout << std::this_thread::get_id() << std::endl;
 
 	// 스레드를 재우려면 인자로 "chrono" 라이브러리 의 시간이 전달되어야 합니다
-	std::this_thread::sleep_for(3s);
+//	std::this_thread::sleep_for(3s);
 	std::this_thread::sleep_for(std::chrono::seconds(3) );
+
+	std::this_thread::sleep_for(std::literals::operator""s(3));
 
 //	std::this_thread::sleep_until(이때까지); // xtime 구조체 형태로 전달.
 
