3801fc41df99ecdd678b6d8524b8181b5b1d86fe
ecourse <smkang@codenuri.co.kr>
2022-04-15 09:24:12 +0900

    Add files via upload

--- DAY5/2_ref5.cpp
+++ DAY5/2_ref5.cpp
@@ -13,7 +13,12 @@ int main()
 						   // 인자로 v 를 보내 달라.
 	t1.join();
 
-	std::thread t2(f2, v); // error.
+//	std::thread t2(f2, v); // error.
+
+	// 결국 v 는 직접 f2로 전달되는 것이 아니라
+	// v => thread 생성자 => f2로 전달되게 되는데
+	// thread 생성자가 call by value 로 되어 있습니다.
+	// call by value 로 된 곳에 "참조로 보내려면 std::ref()" 를 사용해야합니다
 	std::thread t2(f2, std::ref(v)); // ok.
 	t2.join();
 }
