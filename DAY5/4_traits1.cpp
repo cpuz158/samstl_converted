4f9827b61a668946b1eaee652bf289ed12a3a6ff
ecourse <smkang@codenuri.co.kr>
2022-04-15 13:55:02 +0900

    Add files via upload

--- DAY5/4_traits1.cpp
+++ DAY5/4_traits1.cpp
@@ -1,13 +1,29 @@
 ﻿// 4_traits
 #include <iostream>
+#include <type_traits> // C++11 에서 추가된 라이브러리
 
+/*
 template<typename T> void printv(const T& v)
 {
-	if ( T 가 포인터 타입 이면 )
+	if ( std::is_pointer<T>::value  )
 		std::cout << v << " : " << *v << std::endl;
 	else 
 		std::cout << v << std::endl;
 }
+*/
+
+// C++17 의 새로운 if 문 : if constexpr 
+// => 컴파일 시간 if 문, 템플릿 만들때 주로 사용
+// => 조건이 false 인 경우, 실제 C++ 코드(함수, 클래스)를 만들때 포함되지 않는다.
+
+template<typename T> void printv(const T& v)
+{
+	if constexpr (std::is_pointer<T>::value)
+		std::cout << v << " : " << *v << std::endl;
+	else
+		std::cout << v << std::endl;
+}
+
 
 int main()
 {
@@ -15,7 +31,7 @@ int main()
 	double d = 10;
 
 	printv(n);
-	printv(d);
+//	printv(d);
 	printv(&n);
 }
 
